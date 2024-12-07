57c12c829b47a9b185829084b006f8ac67c03dd7
imam hossen <imam.frontend404@gmail.com>
2024-12-07 19:26:37 +0600

    commit

--- src/app/component/header/Countory.js
+++ src/app/component/header/Countory.js
@@ -1,31 +1,28 @@
 import React, { useState } from "react";
 import ReactFlagsSelect from "react-flags-select";
 
-
-export default function Countory() {
+export default function Country() {
   const [select, setSelect] = useState("SE");
+  
   const onSelect = (code) => setSelect(code);
+  
   console.log("SELECT", select);
+
   return (
-    <div className="Countory">
+    <div className="country">
       <ReactFlagsSelect
         className="flagwdi w-[153px] align-left"
         selected={select}
         onSelect={onSelect}
         countries={["US", "GB", "FR", "DE", "IT"]}
-        customLabels={{ US: "English", GB: "Germany", FR: "France", DE: "Delli", IT: "IT" }}
+        customLabels={{
+          US: "English",
+          GB: "Germany",
+          FR: "France",
+          DE: "Delhi", 
+          IT: "Italy" 
+        }}
         placeholder="Select Language"
-        /*showSelectedLabel={showSelectedLabel}
-        selectedSize={selectedSize}
-        showOptionLabel={showOptionLabel}
-        optionsSize={optionsSize}
-        placeholder={placeholder}s
-        searchable={searchable}
-        searchPlaceholder={searchPlaceholder}
-        alignOptionsToRight={alignOptionsToRight}
-        fullWidth={fullWidth}
-        disabled={disabled} */
-        
       />
     </div>
   );
