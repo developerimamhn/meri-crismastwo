45abcd01cbf10d4e6069357207390ef067b5eb29
imam hossen <imam.frontend404@gmail.com>
2024-12-07 19:39:52 +0600

    center

--- src/app/component/body/Page1.js
+++ src/app/component/body/Page1.js
@@ -34,7 +34,7 @@ const Page1 = () => {
                                 <div className="text-center ">
                                     <h1 className="w-[400px] md:w-[300px] xl:w-[350px] 2xl:w-[500px] text-center !leading-[120%] merryHeading text-[45px] sm:text-[50px] md:text-[60px] lg:text-[50px] xl:text-[70px] 2xl:text-[80px] !text-[#000]">Get Some ETH</h1>
                                     <p className="px-[15%] sm:px-0 !text-[#000] elevatePragraph text-[13px] sm:text-[14px] md:text-[12px] lg:text-[14px] xl:text-[15px] 2xl:text-[17px] text-center !leading-[180%] w-fit">Ensure your wallet has enough ETH for the $Christmas token swap. 
-                                    You can purchase ETH directly on MetaMask or buy it on an exchange like Binance, then transfer it to your wallet. This way, you're all set to join in the holiday excitement!
+                                    You can purchase ETH directly on MetaMask or buy it on an exchange like Binance, then transfer it to your wallet. This way, you re all set to join in the holiday excitement!
                                     </p>
                                 </div>
                             </div>
